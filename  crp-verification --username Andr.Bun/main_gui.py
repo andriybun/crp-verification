978ac638bba9da45c2ab44eb1a97554f1d40598b
andr.bun <andr.bun@fcda09e5-fb6a-2b27-a430-8ffb1131a96a>
2010-09-09 21:52:46 +0000

    Started working with threads. Minor changes.

---  crp-verification --username Andr.Bun/main_gui.py
+++  crp-verification --username Andr.Bun/main_gui.py
@@ -3,7 +3,7 @@
 # Modified: 
 # Name:     GUI for comparison of global cropland cover scripts
 
-import time, sys
+import time, sys, thread, threading
 from PyQt4 import QtCore, QtGui
 #from PyQt4.QtCore import *
 #from PyQt4.QtGui import *
@@ -11,18 +11,34 @@ from gui.mainWindow import Ui_MainWindow
 from iterableStruct import iterableStruct
 from runAll import runAll
 
+class runAllThread(threading.Thread):
+    def __init__(self, gui):
+        self.__gui = gui
+        self.__dummy = 0;
+        threading.Thread.__init__(self)
+    def run (self):
+        runAll(self.__gui, self.__dummy)
+
 # main window class
 class mainWin(QtGui.QMainWindow):
     def __init__(self, parent=None):
         QtGui.QWidget.__init__(self, parent)
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
-        QtCore.QObject.connect(self.ui.pushRunAll,QtCore.SIGNAL("clicked()"),self._runAll)
+        QtCore.QObject.connect(self.ui.pushRunAll,QtCore.SIGNAL("clicked()"),self.__runAllInThread)
     
     # run all computations
-    def _runAll(self):
-        runAll(self)
+    def __runAll(self):
+        dummy = 0
+        thread.start_new_thread(runAll, (self, dummy))
+        self.printTextTime('thread finished')
+        #runAll(self)
     
+    def __runAllInThread(self):
+        runAllThread(self).start()
+        #runAllThread().run(self)
+        self.printText('thread finished')
+
     # print status with time to text box
     def printTextTime(self, textToAppend):
         self.ui.textEdit.append(time.strftime("%H:%M:%S", time.localtime()) + '\t' + textToAppend)
